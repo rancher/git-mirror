5ab222d7e7d384cd1e9cd8d14d6362080a69e4dd
Raul Sanchez <rawmind@gmail.com>
2017-04-19 23:12:15 +0200

    Feat [git-analyze]: Read more than one file with filepath wildcard

--- images/git-analyze/code/main.go
+++ images/git-analyze/code/main.go
@@ -7,6 +7,7 @@ import (
 	"regexp"
 	"sync"
 	"time"
+	"path/filepath"
 
 	log "github.com/Sirupsen/logrus"
 )
@@ -17,12 +18,11 @@ const (
 )
 
 func main() {
-	var filepath = flag.String("filepath", "/var/log/nginx/access.log", "the log file to analyze")
+	var file_path = flag.String("filepath", "/var/log/nginx/access.log", "Log files to analyze, wildcard allowed between quotes.")
 	var period = flag.String("period", "24h", "period of time (past to now) to analyze")
 	flag.Parse()
 
-	log.Info("Analyzing ", *filepath)
-	file, err := os.Open(*filepath)
+	files , err := filepath.Glob(*file_path)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -54,58 +54,71 @@ func main() {
 		}
 	}
 
-	// 64Kb buffer should be big enough
-	scanner := bufio.NewScanner(file)
-	var wg sync.WaitGroup
-
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		submatches := logline.FindStringSubmatch(string(line))
-		stats.linesParsed++
-
-		if len(submatches) != 12 {
-			log.Warn(string(line))
-			continue
-		}
-
-		logTime, err := time.Parse("2/Jan/2006:15:04:05 -0700", submatches[1])
+	// Getting stats for every file
+	for _, f := range files {
+		log.Info("Analyzing ", f)
+		file, err := os.Open(f)
 		if err != nil {
-			log.Warn(err)
-			stats.linesSkipped++
-			continue
+			log.Fatal(err)
 		}
 
-		// ensure the log line is in the requested period
-		if !cutoffTime.IsZero() && logTime.Before(cutoffTime) {
-			stats.linesSkipped++
-			continue
-		}
+		// 64Kb buffer should be big enough
+		scanner := bufio.NewScanner(file)
+		var wg sync.WaitGroup
 
-		// ensure the request is a catalog git-upload-pack
-		if !uploadpack.MatchString(submatches[5]) {
-			stats.linesSkipped++
-			continue
-		}
+		for scanner.Scan() {
+			line := scanner.Text()
 
-		// do a whois lookup
-		ip := submatches[4]
-		if _, ok := stats.ipRequests[ip]; !ok {
-			if w := stats.FindWhoisRecord(ip); w == nil {
-				wg.Add(1)
-				go func() {
-					w, err := whois(ip, &wg)
-					stats.whoisRequests++
-					if err == nil {
-						stats.AddWhoisRecord(w)
-					}
-				}()
-				time.Sleep(WhoisRequestPeriod)
+			submatches := logline.FindStringSubmatch(string(line))
+			stats.linesParsed++
+
+			if len(submatches) != 12 {
+				log.Warn(string(line))
+				continue
 			}
+
+			logTime, err := time.Parse("2/Jan/2006:15:04:05 -0700", submatches[1])
+			if err != nil {
+				log.Warn(err)
+				stats.linesSkipped++
+				continue
+			}
+
+			// ensure the log line is in the requested period
+			if !cutoffTime.IsZero() && logTime.Before(cutoffTime) {
+				stats.linesSkipped++
+				continue
+			}
+
+			// ensure the request is a catalog git-upload-pack
+			if !uploadpack.MatchString(submatches[5]) {
+				stats.linesSkipped++
+				continue
+			}
+
+			// do a whois lookup
+			ip := submatches[4]
+			if _, ok := stats.ipRequests[ip]; !ok {
+				if w := stats.FindWhoisRecord(ip); w == nil {
+					wg.Add(1)
+					go func() {
+						w, err := whois(ip, &wg)
+						stats.whoisRequests++
+						if err == nil {
+							stats.AddWhoisRecord(w)
+						}
+					}()
+					time.Sleep(WhoisRequestPeriod)
+				}
+			}
+			stats.ipRequests[ip] = append(stats.ipRequests[ip], logTime)
 		}
-		stats.ipRequests[ip] = append(stats.ipRequests[ip], logTime)
+		file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		wg.Wait()
 	}
-	wg.Wait()
 	stats.SaveWhoisRecords()
 	stats.StopLogging()
 	stats.LogResult()
