990c14ed5a2a07f34c5890f7acb37e3e8709e2c8
Taylor Price <tayworm@gmail.com>
2019-06-14 11:38:37 -0700

    get rid of old stuff

--- packer/aws-deps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install -y git python-setuptools
-git clone https://github.com/LLParse/aws-ec2-assign-elastic-ip /tmp/aws-ec2-assign-elastic-ip
-cd /tmp/aws-ec2-assign-elastic-ip
-sudo python setup.py install
-cd /tmp
-sudo rm -rf aws-ec2-assign-elastic-ip
--- terraform/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-aws_access_key = "<your access key here>"
-aws_secret_key = "<your secret key here>"
-aws_key_name   = "<your ssh key name here>"
