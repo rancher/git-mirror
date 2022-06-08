796ac3f25e4e4e8da738b435cf8134e7a5d79b1a
Lucas Ramage <ramage.lucas@protonmail.com>
2022-06-08 10:23:09 -0400

    Remove sysctl config and simplify fcgiwrap

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ RUN apt-get update && \
     apt-get install -y fcgiwrap \
                        git-core \
                        nginx && \
+    echo 'FCGI_CHILDREN="5"' >> /etc/default/fcgiwrap && \
     chmod +x /usr/local/bin/git-porter
 
 ENTRYPOINT ["/usr/local/bin/git-porter"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-echo "fs.file-max = 12000500" >> /etc/sysctl.conf
-echo "fs.nr_open = 20000500" >> /etc/sysctl.conf
-echo "net.core.somaxconn = 512" >> /etc/sysctl.conf
-echo "# <domain> <type> <item>  <value>" >> /etc/security/limits.d/limits.conf
-echo "    *       soft  nofile  20000" >> /etc/security/limits.d/limits.conf
-echo "    *       hard  nofile  20000" >> /etc/security/limits.d/limits.conf
-
-sysctl -p
-
-ulimit -n 200000
-
-# Turn up services
-echo 'FCGI_CHILDREN="5"' >> /etc/default/fcgiwrap
 service fcgiwrap start
 nginx
-tail -f /var/log/nginx/access.log
