06d5881c4a3d87d5208a9c6cc95150873a2d7ae7
Lucas Ramage <ramage.lucas@protonmail.com>
2022-06-27 14:40:43 -0400

    Re-add git-mirror image

---
+++ mirror/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -eu
+
+TMP=$(mcookie)
+TMP="/tmp/${TMP}"
+
+cd "${TMP}"
+
+git clone "${GIT_REPOSITORY_URL}"
+
+cd "${GIT_REPOSITORY_NAME}"
+
+git remote add local "file:///var/git/${GIT_REPOSITORY_NAME}.git"
+
+git push local "${GIT_DEFAULT_BRANCH}"
+
+rm -rf "${TMP}"
+
